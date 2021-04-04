f38e202f8385f76ea58f94368fb4e4a72bac807a
jps <me@mydomain.com>
2021-04-04 16:04:28 +0100

    Change sqlite to postgres

--- main.py
+++ main.py
@@ -10,17 +10,15 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
 import os
-from dotenv import load_dotenv
 
 app = Flask(__name__)
-load_dotenv(".env")
-app.config['SECRET_KEY'] = os.getenv("MyAPIKey")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
